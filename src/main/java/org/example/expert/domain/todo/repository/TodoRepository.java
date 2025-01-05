8081038e31486685361b1931923ae706277a12b9
dllll2 <wlsdud8658@gmail.com>
2025-01-05 11:05:13 +0900

    레벨 4: N+1 문제 개선하기

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -3,11 +3,19 @@ package org.example.expert.domain.todo.repository;
 import org.example.expert.domain.todo.entity.Todo;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 
-    Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
+    @EntityGraph(attributePaths = {"user"})
+    @Query("SELECT t FROM Todo t WHERE t.id = :todoId")
+    Optional<Todo> findByIdWithUser(@Param("todoId") long todoId);
 
-    int countById(Long todoId);
+    @EntityGraph(attributePaths = {"user"})
+    Page<Todo> findAllWithUserByOrderByModifiedAtDesc(Pageable pageable);
 }
